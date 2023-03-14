6592741737b91170fa460345c1e8bc4d80f46447
Rafael <68768478+Rafaelfspenha@users.noreply.github.com>
2023-03-14 01:52:55 -0300

    Update coletando_imagens.py

--- coletando_imagens.py
+++ coletando_imagens.py
@@ -1,21 +1,72 @@
 import time
 import pyautogui
 
+# em chrome://settings/downloads,
+# a opção "perguntar onde salvar o arquivo antes de fazer download" deve estar ativada.
+
 # deixe a tela do chrome a 90%.
-# escolher no google imagens tamanho grande.
-# salvar um arquivo na pasta onde deseja salvar.
-# aperte na primeira imagem.
+# feche todas as janelas do chrome.
+
 j = 1
 k = 1
-while k <= 200:
 
-    pyautogui.sleep(3)
+opcao1 = input("Digite tema a pesquisar: ")
+
+pyautogui.press('win')
+pyautogui.write('chrome')
+pyautogui.sleep(3)
+pyautogui.press('enter')
+pyautogui.sleep(3)
+pyautogui.hotkey('alt','space','x') # maximizar janela do chrome
+pyautogui.sleep(3)
+pyautogui.press('esc')  # essa linha é para evitar erro!
+pyautogui.moveTo(200,50)  # essa linha é para evitar erro!
+pyautogui.click(button='left')  # essa linha é para evitar erro!
+pyautogui.sleep(3)
+pyautogui.write('https://images.google.com/')
+pyautogui.sleep(3)
+pyautogui.press('enter')
+pyautogui.sleep(3)
+pyautogui.write(opcao1)
+pyautogui.sleep(3)
+pyautogui.press('enter')
+pyautogui.sleep(3)
+
+i = 1
+while i <= 16:
+    pyautogui.press("tab")
+    i += 1
+
+pyautogui.sleep(3)
+pyautogui.press('enter')
+pyautogui.sleep(3)
+pyautogui.press("tab")
+pyautogui.press("tab")
+pyautogui.sleep(3)
+
+i = 1
+while i <= 4:
+    pyautogui.press("up")
+    i += 1
+
+pyautogui.sleep(3)
+pyautogui.press('enter')
+pyautogui.sleep(6)
+pyautogui.moveTo(50,300)
+pyautogui.click(button='left')
+
+
+while k <= 2:
+
+    pyautogui.sleep(6)
     pyautogui.moveTo(750,300)
     # x se move da esquerda para direita,y é se move de cima para baixo.
     # no x o canto esquerdo da tela é 0 e no y a parte de cima é 0.
+    pyautogui.sleep(3)
     pyautogui.click(button='right')
     pyautogui.sleep(3)
 
+    # na página do google maps, após clicar na foto
     i = 1
     while i <=6:
         pyautogui.press("up")
@@ -25,7 +76,11 @@ while k <= 200:
     pyautogui.press('enter')
     pyautogui.sleep(2)
     pyautogui.hotkey('ctrl','tab')
+
+    # na próxima guia
+    pyautogui.sleep(3)
     pyautogui.moveTo(500, 300)
+    pyautogui.sleep(3)
     pyautogui.click(button='right')
     pyautogui.sleep(3)
 
@@ -36,22 +91,23 @@ while k <= 200:
 
     pyautogui.sleep(3)
     pyautogui.press('enter')
+    pyautogui.sleep(3)
     pyautogui.press('left')
+    pyautogui.sleep(3)
 
     if j % 2 == 0:
         pyautogui.write('a')
     else:
         pyautogui.write('b')
-    j += 1
+    j+=1
 
+    pyautogui.sleep(3)
     pyautogui.press('enter')
     pyautogui.sleep(3)
     pyautogui.hotkey('ctrl','w')
+    pyautogui.sleep(3)
     pyautogui.press('right')
 
     k += 1
-
-
-
 
 
